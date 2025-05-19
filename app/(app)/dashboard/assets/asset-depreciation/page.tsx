2d4b44fbe2d4390da237560d7093fae68682a161
Akil Tajwar <akiltajwar@gmail.com>
2025-05-19 14:08:50 +0600

    wip: asset-depreciation

---
+++ app/(app)/dashboard/assets/asset-depreciation/page.tsx
@@ -0,0 +1,12 @@
+import AssetDepreciation from '@/components/dashboard/assets/asset-depreciation/asset-depreciation'
+import React from 'react'
+
+const AssetDepreciationPage = () => {
+  return (
+    <div>
+      <AssetDepreciation />
+    </div>
+  )
+}
+
+export default AssetDepreciationPage
